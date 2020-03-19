9b59f7fe5432d4597bac899d2586b615d835e909
MatsuTaku <tkm.matsumoto@gmail.com>
2020-03-19 18:57:24 +0900

    Update benchmark environment.

---
+++ README.md
@@ -0,0 +1,19 @@
+# bit-fitting
+
+## Benchmark usage
+```
+# Build program
+mkdir build
+cd build
+cmake ..
+make
+cd ..
+
+# Setup to plot benchmark resuit
+python -m venv env
+source env/bin/activate
+pip install -r requirements.txt
+
+# Run benchmark
+build/bench | tee >(python plot_bench.py)
+```
--- plot_bench.py
+++ plot_bench.py
@@ -1,5 +1,4 @@
 import sys
-import numpy
 import matplotlib.pyplot as plt
 
 process_id = 0
@@ -15,16 +14,17 @@ for line in sys.stdin:
         process_id = 2
     elif process_id == 2:
         datas = line.strip().split('\t')
-        for (i, data) in enumerate(datas):
-            alg_datas[i].append(int(data))
+        alg_datas[0].append(int(datas[0]))
+        for (i, data) in enumerate(datas[1:]):
+            alg_datas[i+1].append(float(data))
 
 
 fig, ax = plt.subplots()
 for i in range(len(alg_names)):
     ax.plot(alg_datas[0], alg_datas[i+1], label=alg_names[i])
 ax.set_xlabel('alphabet size')
-ax.set_ylabel('time [us/query]')
 ax.set_xscale('log')
+ax.set_ylabel('time [us/query]')
 ax.set_yscale('log')
 ax.legend()
 
